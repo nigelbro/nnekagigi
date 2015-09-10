682fb5d3f36b41e2ff128873643fe823f5c5929a
nigelbro <nigelbro@buffalo.edu>
2015-09-10 08:27:49 -0400

    extra caret deletion

--- nnekafooter.php
+++ nnekafooter.php
@@ -23,7 +23,7 @@ echo <<<_END
 </div>
 
 </div>
-<<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.2/jquery.min.js"></script>
 <script src="bootstrap/js/bootstrap.min.js"></script>
 <script src="jquery/setUpModalItem.js"></script>
 </body>
